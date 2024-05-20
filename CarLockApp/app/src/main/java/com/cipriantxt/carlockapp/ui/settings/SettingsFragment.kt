bdd5cba5a281d8dfd0e6943cb11d9ca6ec821935
CiprianTXT <mc.popescu2002@gmail.com>
2024-05-20 05:09:16 +0300

    Added functionality to fragment elements

--- CarLockApp/app/src/main/java/com/cipriantxt/carlockapp/ui/settings/SettingsFragment.kt
+++ CarLockApp/app/src/main/java/com/cipriantxt/carlockapp/ui/settings/SettingsFragment.kt
@@ -4,49 +4,68 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.findNavController
+import com.cipriantxt.carlockapp.ActivityPipe
+import com.cipriantxt.carlockapp.R
 import com.cipriantxt.carlockapp.databinding.FragmentSettingsBinding
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
 class SettingsFragment : Fragment() {
-
     private var _binding: FragmentSettingsBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
+    private var activityPipe: ActivityPipe? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val settingsViewModel =
-            ViewModelProvider(this).get(SettingsViewModel::class.java)
-
         _binding = FragmentSettingsBinding.inflate(inflater, container, false)
         val root: View = binding.root
-//        For this component:
-//        <TextView
-//        android:id="@+id/text_settings"
-//        android:layout_width="match_parent"
-//        android:layout_height="wrap_content"
-//        android:layout_marginStart="8dp"
-//        android:layout_marginTop="8dp"
-//        android:layout_marginEnd="8dp"
-//        android:textAlignment="center"
-//        android:textSize="20sp"
-//        app:layout_constraintBottom_toBottomOf="parent"
-//        app:layout_constraintEnd_toEndOf="parent"
-//        app:layout_constraintStart_toStartOf="parent"
-//        app:layout_constraintTop_toTopOf="parent" />
 
-//        Modifying content:
-//        val textView: TextView = binding.textSettings
-//        settingsViewModel.text.observe(viewLifecycleOwner) {
-//            textView.text = it
-//        }
+        activityPipe = requireActivity() as ActivityPipe
+        val deviceSaveBtn = binding.deviceSaveBtn
+        deviceSaveBtn.setOnClickListener { view ->
+            val deviceName =  if (binding.deviceNameField.text!!.isNotEmpty()) {
+                binding.deviceNameField.text
+            } else {
+                "*"
+            }
+            val devicePin = if (binding.devicePinField.text!!.isNotEmpty()) {
+                binding.devicePinField.text
+            } else {
+                "*"
+            }
+            val deviceCredCommand = "setBtCred:$deviceName,$devicePin"
+
+            if (deviceCredCommand != "setBtCred:*,*") {
+                MaterialAlertDialogBuilder(requireContext())
+                    .setTitle(R.string.device_save_dialog_title)
+                    .setMessage(R.string.device_save_dialog_desc)
+                    .setNegativeButton(R.string.device_save_dialog_negative) { dialog, _ ->
+                        dialog.dismiss()
+                    }
+                    .setPositiveButton(R.string.device_save_dialog_positive) { _, _ ->
+                        try {
+                            val btConnection = activityPipe!!.getBtConnection()
+                            btConnection!!.enqueueJob(this@SettingsFragment, deviceCredCommand)
+                        } catch (e: Exception) {
+                            when (e) {
+                                is NullPointerException, is ClassCastException -> {
+                                    Toast.makeText(requireContext(), R.string.bt_no_remote_connection, Toast.LENGTH_SHORT).show()
+                                }
+                            }
+                        } finally {
+                            view.findNavController().navigate(R.id.navigation_home)
+                        }
+                    }
+                    .show()
+            } else {
+                Toast.makeText(requireContext(), R.string.device_empty_fields, Toast.LENGTH_SHORT).show()
+            }
+        }
         return root
     }
 
